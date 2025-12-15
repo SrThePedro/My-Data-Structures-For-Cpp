23a53aa7626c4987ccf70e2e509ce6d0b3894e58
slhemnkr <slhemnkr@gmail.com>
2025-12-15 18:53:20 +0300

    add print head, tail, count, list and append functions

--- MyDataStructuresForCpp/linkedList.h
+++ MyDataStructuresForCpp/linkedList.h
@@ -12,6 +12,49 @@ public:
 		value = _value;
 		next = NULL;
 	}
+	friend class LinkedList;
+};
+
+class LinkedList {
+private:
+	Node* head;
+	Node* tail;
+	int count = 0;
+
+public:
+	LinkedList(int _value) {
+		Node* node = new Node(_value);
+		head = node;
+		tail = node;
+		count = 1;
+	}
+
+	void printHead() {
+		cout << endl << "Head of list: " << head->value;
+	}
+
+	void printTail() {
+		cout << endl << "Tail of list: " << tail->value;
+	}
+
+	void printCount() {
+		cout << endl << "Count: " << count;
+	}
+
+	void append(int value) {
+		Node* newnode = new Node(value);
+		tail->next = newnode;
+		tail = newnode;
+		count++;
+	}
+
+	void printList() {
+		Node* temp = head;
+		while (temp != NULL){
+			cout << temp->value << " ";
+			temp = temp->next;
+		}
+	}
 };
 
 
--- MyDataStructuresForCpp/main.cpp
+++ MyDataStructuresForCpp/main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 
 int main() {
+	LinkedList list1(10);
+
+	list1.append(9);
+	list1.append(8);
+	list1.append(7);
+	list1.append(6);
+	list1.append(5);
+
+	list1.printHead();
+	list1.printTail();
+	list1.printCount();
+	cout << endl << endl;
+	list1.printList();
+
+
+
 
 
 	return 0;
